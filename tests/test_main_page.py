1e8d5aecffceecbdc1e24d665519296feb35d5d4
vrtpoint <vrtpoint@yahoo.com>
2019-12-15 22:32:47 +0300

    fix/added opening page method

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -5,24 +5,28 @@ data = setUpConfig()
 
 def test_adding_to_wish_list(driver):
     """Тест добавления позиции в список желаемых покупок"""
+    driver.open(data['url'])
     driver.authorization.login(data['app_username'], data['app_password'])
     driver.products.add_to_wish_list()
     driver.authorization.logout()
 
 def test_removal_position_from_wish_list(driver):
     """Тест удаления позиции из списка желаемых покупок"""
+    driver.open(data['url'])
     driver.authorization.login(data['app_username'], data['app_password'])
     driver.products.remove_from_wish_list()
     driver.authorization.logout()
 
 def test_adding_to_shopping_cart(driver):
     """Тест добавления позиции в корзину покупок"""
+    driver.open(data['url'])
     driver.authorization.login(data['app_username'], data['app_password'])
     driver.products.add_to_shopping_cart()
     driver.authorization.logout()
 
 def test_removal_position_from_shopping_cart(driver):
     """Тест удаления позиции из корзины покупок"""
+    driver.open(data['url'])
     driver.authorization.login(data['app_username'], data['app_password'])
     driver.products.remove_position_from_shopping_cart()
     driver.authorization.logout()
