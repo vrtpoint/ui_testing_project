baa0ac1bf3fa6e47463e7d49a7d7797a6e6d3235
vrtpoint <vrtpoint@yahoo.com>
2019-12-16 07:57:17 +0300

    replaced config's settings with a new one & added to the .gitignore

--- config_example.py
+++
@@ -1,9 +0,0 @@
-def setUpConfig():
-    data = {
-        'url': 'url',
-        'app_username': 'username',
-        'app_password': 'password',
-        'admin_username': 'username',
-        'admin_password': 'password'
-    }
-    return data
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,33 +1,33 @@
 from config import setUpConfig
-
+from decouple import config
 
 data = setUpConfig()
 
 def test_adding_to_wish_list(driver):
     """Тест добавления позиции в список желаемых покупок"""
-    driver.open(data['url'])
-    driver.authorization.login(data['app_username'], data['app_password'])
+    driver.open(config('url'))
+    driver.authorization.login(config('app_username'), config('app_password'))
     driver.products.add_to_wish_list()
     driver.authorization.logout()
 
 def test_removal_position_from_wish_list(driver):
     """Тест удаления позиции из списка желаемых покупок"""
-    driver.open(data['url'])
-    driver.authorization.login(data['app_username'], data['app_password'])
+    driver.open(config('url'))
+    driver.authorization.login(config('app_username'), config('app_password'))
     driver.products.remove_from_wish_list()
     driver.authorization.logout()
 
 def test_adding_to_shopping_cart(driver):
     """Тест добавления позиции в корзину покупок"""
-    driver.open(data['url'])
-    driver.authorization.login(data['app_username'], data['app_password'])
+    driver.open(config('url'))
+    driver.authorization.login(config('app_username'), config('app_password'))
     driver.products.add_to_shopping_cart()
     driver.authorization.logout()
 
 def test_removal_position_from_shopping_cart(driver):
     """Тест удаления позиции из корзины покупок"""
-    driver.open(data['url'])
-    driver.authorization.login(data['app_username'], data['app_password'])
+    driver.open(config('url'))
+    driver.authorization.login(config('app_username'), config('app_password'))
     driver.products.remove_position_from_shopping_cart()
     driver.authorization.logout()
 
