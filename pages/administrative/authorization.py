b736f7cc0226d3dfb046555fef03ffe76f79cee1
vrtpoint <vrtpoint@yahoo.com>
2019-12-23 07:37:25 +0300

    edited authorization's methods for tests & deleted useless files

--- pages/administrative/authorization.py
+++ pages/administrative/authorization.py
@@ -2,14 +2,17 @@ from locators.administrative.product_page import LoginProductPage, AdminPage
 from pages.helpers.base_actions import BaseActions
 
 
-class Authorization(BaseActions):
+class AdminPageAuthorization(BaseActions):
+
+        auth = LoginProductPage
+        deauth = AdminPage
 
         def login(self, app_username, app_password):
-            self.input(*self.LoginProductPage.login_field, value=app_username)
-            self.input(*self.LoginProductPage.password_field, value=app_password)
-            self.click(*self.LoginProductPage.login_button)
-            assert self.wd.find_element(*self.LoginProductPage.dashboard_text).text == 'Dashboard'
+            self.input(*self.auth.login_field, value=app_username)
+            self.input(*self.auth.password_field, value=app_password)
+            self.click(*self.auth.login_button)
+            #assert self.wd.find_element(*self.auth.entry_title).text == 'Dashboard'
 
         def logout(self):
-            self.click(*self.AdminPage.logout_button)
+            self.click(*self.deauth.logout_button)
             assert self.wd.find_element(*self.LoginProductPage.login_button).is_displayed()
