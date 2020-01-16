8c556595e7bb349661cf31f3393adce278eae0b3
vrtpoint <vrtpoint@yahoo.com>
2020-01-16 11:15:42 +0300

    encapsulated methods in the BaseActions class

--- pages/helpers/base_actions.py
+++ pages/helpers/base_actions.py
@@ -5,21 +5,21 @@ from selenium.webdriver.support.ui import WebDriverWait
 class BaseActions:
 
     def __init__(self, app):
-        self.app = app
-        self.wd = self.app.wd
-        self.wait = WebDriverWait(self.wd, 30)
+        self.__app = app
+        self.__wd = self.__app.wd
+        self.__wait = WebDriverWait(self.__wd, 30)
 
-    def searching_element(self, type_selector, selector):
-        self.wait.until(EC.visibility_of_element_located((type_selector, selector)))
-        self.wait.until(EC.element_to_be_clickable((type_selector, selector)))
-        return self.wd.find_element(type_selector, selector)
+    def __searching_element(self, type_selector, selector):
+        self.__wait.until(EC.visibility_of_element_located((type_selector, selector)))
+        self.__wait.until(EC.element_to_be_clickable((type_selector, selector)))
+        return self.__wd.find_element(type_selector, selector)
 
-    def waiting_for_element(self, type_selector, selector):
-        self.wait.until(EC.visibility_of_element_located((type_selector, selector)))
+    def __waiting_for_element(self, type_selector, selector):
+        self.__wait.until(EC.visibility_of_element_located((type_selector, selector)))
 
-    def click(self, type_selector, selector):
-        self.searching_element(type_selector, selector).click()
+    def __click(self, type_selector, selector):
+        self.__searching_element(type_selector, selector).click()
 
-    def input(self, type_selector, selector, value):
-        self.searching_element(type_selector, selector).clear()
-        self.searching_element(type_selector, selector).send_keys(value)
+    def __input(self, type_selector, selector, value):
+        self.__searching_element(type_selector, selector).clear()
+        self.__searching_element(type_selector, selector).send_keys(value)
