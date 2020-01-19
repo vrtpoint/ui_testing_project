cd1beaee8ae9bf8536538110ec1dbb313b5375d2
vrtpoint <vrtpoint@yahoo.com>
2020-01-19 22:28:55 +0300

    encapsulated methods for the authorizaion's methods; added spaces for readability

--- pages/authorization.py
+++ pages/authorization.py
@@ -7,14 +7,18 @@ class ApplicationAuthorization(BaseActions):
         auth = MainPage
 
         def login(self, app_username, app_password):
-            self.click(*self.auth.HeaderSection.my_account_link)
-            self.click(*self.auth.HeaderSection.login_link)
-            self.input(*self.auth.HeaderSection.email_address_field, value=app_username)
-            self.input(*self.auth.HeaderSection.password_field, value=app_password)
-            self.click(*self.auth.HeaderSection.login_button)
-            assert self.wd.find_element(*self.auth.InnerSection.account_breadcrumb).text == 'Account'
+            self._click(*self.auth.HeaderSection.my_account_link)
+            self._click(*self.auth.HeaderSection.login_link)
+            self._input(*self.auth.HeaderSection.email_address_field, value=app_username)
+            self._input(*self.auth.HeaderSection.password_field, value=app_password)
+            self._click(*self.auth.HeaderSection.login_button)
+
+            assert self._wd \
+                .find_element(*self.auth.InnerSection.account_breadcrumb).text == 'Account'
 
         def logout(self):
-            self.click(*self.auth.HeaderSection.my_account_link)
-            self.click(*self.auth.HeaderSection.logout_button)
-            assert self.wd.find_element(*self.auth.InnerSection.logout_breadcrumb).text == 'Logout'
+            self._click(*self.auth.HeaderSection.my_account_link)
+            self._click(*self.auth.HeaderSection.logout_button)
+
+            assert self._wd \
+               .find_element(*self.auth.InnerSection.logout_breadcrumb).text == 'Logout'
