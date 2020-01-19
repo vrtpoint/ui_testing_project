8de8f93134b6dc85690ad694eed11047ad2571e3
vrtpoint <vrtpoint@yahoo.com>
2020-01-19 22:25:44 +0300

    encapsulated methods for the base actions

--- pages/helpers/base_actions.py
+++ pages/helpers/base_actions.py
@@ -6,20 +6,20 @@ class BaseActions:
 
     def __init__(self, app):
         self.__app = app
-        self.__wd = self.__app.wd
-        self.__wait = WebDriverWait(self.__wd, 30)
+        self._wd = self.__app.wd
+        self.__wait = WebDriverWait(self._wd, 30)
 
     def __searching_element(self, type_selector, selector):
         self.__wait.until(EC.visibility_of_element_located((type_selector, selector)))
         self.__wait.until(EC.element_to_be_clickable((type_selector, selector)))
-        return self.__wd.find_element(type_selector, selector)
+        return self._wd.find_element(type_selector, selector)
 
     def __waiting_for_element(self, type_selector, selector):
         self.__wait.until(EC.visibility_of_element_located((type_selector, selector)))
 
-    def __click(self, type_selector, selector):
+    def _click(self, type_selector, selector):
         self.__searching_element(type_selector, selector).click()
 
-    def __input(self, type_selector, selector, value):
+    def _input(self, type_selector, selector, value):
         self.__searching_element(type_selector, selector).clear()
         self.__searching_element(type_selector, selector).send_keys(value)
