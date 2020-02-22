fff14d81bdc11b7638fba5077bccec43a2e739b4
vrtpoint <vrtpoint@yahoo.com>
2020-02-22 19:03:47 +0300

    made a special classes for the test; used driver from the separated catalog; used page's files in the tests

--- tests/test_shopping_cart.py
+++ tests/test_shopping_cart.py
@@ -1,19 +1,28 @@
+from tests.common.base_set_up import TestBaseSetUp
+from pages.authorization import ApplicationAuthorization
+from pages.shopping_cart import ShoppingCart
 from decouple import config
 import allure
 
 
-@allure.feature('Работа с корзиной')
-def test_adding_to_shopping_cart(driver):
-    driver.open(config('url'))
-    driver.authorization.login(config('app_username'), config('app_password'))
-    driver.shopping_cart.add_to_shopping_cart()
-    driver.authorization.logout()
+class TestShoppingCart(TestBaseSetUp):
 
-@allure.feature('Работа с корзиной')
-def test_removal_position_from_shopping_cart(driver):
-    driver.open(config('url'))
-    driver.authorization.login(config('app_username'), config('app_password'))
-    driver.shopping_cart.remove_position_from_shopping_cart()
-    driver.authorization.logout()
+    @allure.feature('Работа с корзиной')
+    def test_adding_to_shopping_cart(self):
+        self.driver.get(config('url'))
+        auth_page = self.get_page(ApplicationAuthorization)
+        auth_page.login(config('app_username'), config('app_password'))
+        shopping_page = self.get_page(ShoppingCart)
+        shopping_page.add_to_shopping_cart()
+        auth_page.logout()
+
+    @allure.feature('Работа с корзиной')
+    def test_removal_position_from_shopping_cart(self):
+        self.driver.get(config('url'))
+        auth_page = self.get_page(ApplicationAuthorization)
+        auth_page.login(config('app_username'), config('app_password'))
+        shopping_page = self.get_page(ShoppingCart)
+        shopping_page.remove_position_from_shopping_cart()
+        auth_page.logout()
 
 
--- tests/test_wish_list.py
+++ tests/test_wish_list.py
@@ -1,20 +1,29 @@
+from tests.common.base_set_up import TestBaseSetUp
+from pages.authorization import ApplicationAuthorization
+from pages.wish_list import WishList
 from decouple import config
 import allure
 
+class TestWishList(TestBaseSetUp):
 
-@allure.feature('Работа cо списком желаемых покупок')
-def test_adding_to_wish_list(driver):
-    driver.open(config('url'))
-    driver.authorization.login(config('app_username'), config('app_password'))
-    driver.wish_list.add_to_wish_list()
-    driver.authorization.logout()
+    @allure.feature('Работа cо списком желаемых покупок')
+    def test_adding_to_wish_list(self):
+        self.driver.get(config('url'))
+        auth_page = self.get_page(ApplicationAuthorization)
+        auth_page.login(config('app_username'), config('app_password'))
+        wish_list_page = self.get_page(WishList)
+        wish_list_page.add_to_wish_list()
+        auth_page.logout()
 
-@allure.feature('Работа cо списком желаемых покупок')
-def test_removal_position_from_wish_list(driver):
-    driver.open(config('url'))
-    driver.authorization.login(config('app_username'), config('app_password'))
-    driver.wish_list.remove_from_wish_list()
-    driver.authorization.logout()
+    @allure.feature('Работа cо списком желаемых покупок')
+    def test_removal_position_from_wish_list(self):
+        self.driver.get(config('url'))
+        auth_page = self.get_page(ApplicationAuthorization)
+        auth_page.login(config('app_username'), config('app_password'))
+        wish_list_page = self.get_page(WishList)
+        wish_list_page.remove_from_wish_list()
+        auth_page.logout()
+
 
 
 
