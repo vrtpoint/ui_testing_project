15d9bc4c78d7092591f229201e3565547ceee389
workstation <vrtpoint@yahoo.com>
2020-02-25 10:16:35 +0300

    made the test active

--- tests/test_shopping_cart.py
+++ tests/test_shopping_cart.py
@@ -16,13 +16,13 @@ class TestShoppingCart(TestBaseSetUp):
         shopping_page.add_to_shopping_cart()
         auth_page.logout()
 
-    # @allure.feature('Работа с корзиной')
-    # def test_removal_position_from_shopping_cart(self):
-    #     self.driver.get(config('url'))
-    #     auth_page = self.get_page(ApplicationAuthorizationPage)
-    #     auth_page.login(config('app_username'), config('app_password'))
-    #     shopping_page = self.get_page(ShoppingCartPage)
-    #     shopping_page.remove_position_from_shopping_cart()
-    #     auth_page.logout()
+    @allure.feature('Работа с корзиной')
+    def test_removal_position_from_shopping_cart(self):
+        self.driver.get(config('url'))
+        auth_page = self.get_page(ApplicationAuthorizationPage)
+        auth_page.login(config('app_username'), config('app_password'))
+        shopping_page = self.get_page(ShoppingCartPage)
+        shopping_page.remove_position_from_shopping_cart()
+        auth_page.logout()
 
 
