892fb2ca2deab01420139e21136ca148c63d40e4
vrtpoint <vrtpoint@yahoo.com>
2020-02-25 07:45:16 +0300

    edited condition of the fixture

--- tests/common/base_set_up.py
+++ tests/common/base_set_up.py
@@ -3,7 +3,7 @@ import pytest
 
 class TestBaseSetUp:
 
-    @pytest.fixture(scope='class')
+    @pytest.fixture(autouse=True)
     def action_driver(self, driver):
         self.driver = driver
         return self.driver
